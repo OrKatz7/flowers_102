48501f70f8ae611229ab2aed7e33a5ce26d44dc3
OrKatz7 <Or.Katz1@gmail.com>
2022-07-10 15:16:01 +0300

    Update run.sh

--- part2/run.sh
+++ part2/run.sh
@@ -1,24 +1,24 @@
-# /home/kaor/.conda/envs/openmmlab/bin/python main.py --config levit_384
-/home/kaor/.conda/envs/openmmlab/bin/python main.py --config swin_base_patch4_window12_384 --train --test --seed 42
-/home/kaor/.conda/envs/openmmlab/bin/python main.py --config swin_base_patch4_window12_384 --train --test --seed 77
+python main.py --config levit_384
+python main.py --config swin_base_patch4_window12_384 --train --test --seed 42
+python main.py --config swin_base_patch4_window12_384 --train --test --seed 77
 
-/home/kaor/.conda/envs/openmmlab/bin/python main.py --config swin_large_patch4_window12_384 --train --test --seed 42
-/home/kaor/.conda/envs/openmmlab/bin/python main.py --config swin_large_patch4_window12_384 --train --test --seed 77
+python main.py --config swin_large_patch4_window12_384 --train --test --seed 42
+python main.py --config swin_large_patch4_window12_384 --train --test --seed 77
 
-/home/kaor/.conda/envs/openmmlab/bin/python main.py --config swinv2_large_window12to24_192to384_22kft1k --train --test --seed 42
-/home/kaor/.conda/envs/openmmlab/bin/python main.py --config swinv2_large_window12to24_192to384_22kft1k --train --test --seed 77
-
-
-/home/kaor/.conda/envs/openmmlab/bin/python main.py --config volo_d4_448 --train --test --seed 42
-/home/kaor/.conda/envs/openmmlab/bin/python main.py --config volo_d4_448 --train --test --seed 77
+python main.py --config swinv2_large_window12to24_192to384_22kft1k --train --test --seed 42
+python main.py --config swinv2_large_window12to24_192to384_22kft1k --train --test --seed 77
 
 
-/home/kaor/.conda/envs/openmmlab/bin/python main.py --config convnext_base --train --test --seed 42
-/home/kaor/.conda/envs/openmmlab/bin/python main.py --config convnext_base --train --test --seed 77
+python main.py --config volo_d4_448 --train --test --seed 42
+python main.py --config volo_d4_448 --train --test --seed 77
 
 
-/home/kaor/.conda/envs/openmmlab/bin/python main.py --config tf_efficientnet_b5_ns --train --test --seed 42
-/home/kaor/.conda/envs/openmmlab/bin/python main.py --config tf_efficientnet_b5_ns --train --test --seed 77
+python main.py --config convnext_base --train --test --seed 42
+python main.py --config convnext_base --train --test --seed 77
+
+
+python main.py --config tf_efficientnet_b5_ns --train --test --seed 42
+python main.py --config tf_efficientnet_b5_ns --train --test --seed 77
 
 
 
